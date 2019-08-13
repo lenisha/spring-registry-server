1016f19363303300054e003356902aab5799dcfa
Elena Neroslavskaya <lenisha@gmail.com>
2019-08-13 15:27:41 -0400

    init

---
+++ src/main/java/com/ops/monitor/regitryserver/RegitryServerApplication.java
@@ -0,0 +1,13 @@
+package com.ops.monitor.regitryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegitryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegitryServerApplication.class, args);
+	}
+
+}
