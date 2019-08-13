5d6124466b2b0c8908dca09bb9f7be4867ad87d5
Elena Neroslavskaya <lenisha@gmail.com>
2019-08-13 18:30:34 -0400

    eureka server

--- src/main/java/com/ops/monitor/regitryserver/RegistryServerApplication.java
+++ src/main/java/com/ops/monitor/regitryserver/RegistryServerApplication.java
@@ -2,12 +2,14 @@ package com.ops.monitor.regitryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class RegitryServerApplication {
+@EnableEurekaServer
+public class RegistryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegitryServerApplication.class, args);
+		SpringApplication.run(RegistryServerApplication.class, args);
 	}
 
 }
